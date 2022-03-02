ae74c3c2a88b538aedac9016fb35deb45d1f7bde
Ingar Solveigson Asheim <ingara@stud.ntnu.no>
2022-03-02 09:53:57 +0100

    Main was not added to previous commit.

---
+++ src/main/java/project/Main.java
@@ -0,0 +1,17 @@
+package project;
+
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main
+ {
+    public static void main(String[] args) {
+        BufferedReader reader = new BufferedReader(
+            new InputStreamReader(System.in));
+
+        String foo = reader.readLine();
+
+        System.out.println(foo);
+    }
+}
