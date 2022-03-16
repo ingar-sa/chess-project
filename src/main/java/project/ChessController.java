129611133c16c99d16738f397564eb41ba25d875
Ingar Solveigson Asheim <ingara@stud.ntnu.no>
2022-03-16 17:55:31 +0100

    Fixed graphics showing when running app.

--- src/main/java/project/ChessController.java
+++ src/main/java/project/ChessController.java
@@ -1,21 +1,19 @@
 package project;
+
 import javafx.fxml.FXML;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.GridPane;
 
-
 public class ChessController {
-
-    @FXML
-    private GridPane sprites;
-
-    @FXML
-    private GridPane tileColor;
-
-    private Chess game;
     
-    private void initChess() {
-        game = new Chess();
-    }
+    @FXML
+    private GridPane tileColors;
 
+    @FXML
+    private GridPane tileGrid;
 
+    @FXML
+    private ImageView sprite00, sprite01, sprite02, sprite03, sprite04, sprite05, sprite06, sprite07;
+    
+    
 }
