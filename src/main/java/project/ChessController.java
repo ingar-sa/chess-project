58974f512b2aa4a8e62feb196435d21628d5c256
Ingar Solveigson Asheim <ingara@stud.ntnu.no>
2022-03-20 15:39:43 +0100

    Made terminal chess it's own thing. Decided to change keys in legal moves hashmap to the tiles themselves.

--- src/main/java/project/ChessController.java
+++ src/main/java/project/ChessController.java
@@ -1,7 +1,5 @@
 package project;
 
-
-
 import java.time.Period;
 
 import javax.crypto.spec.IvParameterSpec;
--- src/main/java/project/TerminalChess.java
+++ src/main/java/project/TerminalChess.java
@@ -72,7 +72,7 @@ public class TerminalChess {
                     keyForPieceToMove = Pieces; 
                     legalPiece = true;
                 }
-
+            
             }
 
             if (!legalPiece) {
