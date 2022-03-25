7ce9ed57008e98aa4dd809864029b80ef3803f45
Ingar Solveigson Asheim <ingara@stud.ntnu.no>
2022-03-25 17:00:05 +0100

    Started working on controller and game. We are now able to select a piece, and can perform logic from there in the future.

--- src/main/java/project/ChessController.java
+++ src/main/java/project/ChessController.java
@@ -1,6 +1,7 @@
 package project;
 
 import java.time.Period;
+import java.util.ArrayList;
 
 import javax.crypto.spec.IvParameterSpec;
 
@@ -11,6 +12,8 @@ import javafx.scene.input.MouseButton;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.Pane;
+import javafx.scene.shape.Circle;
+import project.Pieces.Piece;
 
 /*
 
@@ -21,14 +24,57 @@ THE ROWS IN THE JAVAFX CHESSBOARD STARTS WITH 0 AT THE TOP,  NOT THE BOTTOM LIKE
 Translate to our coordinate system -> ourY = 7 - guiY
 Translate to their coordinate system -> guiY = 7 - ourY
 
+   Necessary data
+
+    which player has the current turn (currentPlayerTurn) = white
+    has a piece already been chosen (hasPieceBeenChosen) = false
+    which piece has been chosen (pieceChosen) = null;
+    string of legal moves used to draw circles on tiles
+
+    ImageView.onclick() {
+
+        if !hasPieceBeenChosen:
+
+            if !(piece || ourColor):
+                do nothing
+
+            get coordinates
+            get url // what image the piece is
+
+            send coordinate to Game // Game creates a variable 
+
+            get a string in the form of "123446" representing legal moves
+            draw circles on legal moves
+        
+            hasPieceBeenChosen = true;
+
+            return;
+
+        else:
+            //a piece is already chosen
+            get coordinates
+            (maybe) send coordinates to Game
+            
+            if coordinate in legal moves:
+                en passant and castling needs to move two pieces
+                send coordinates of new tile to Game
+                move piece from old tile
+
+            Check with game if pawn has promoted 
+                fix queen promotion
+
+            hasPieceBeenChosen = false
+            currentPlayerTurn = opposite color
+            remove all circles drawn
+            return;
+    }
+
+
 */
 
 public class ChessController {
 
 
-
-    
-    
     @FXML
     private GridPane tileColors;
 
@@ -46,37 +92,84 @@ public class ChessController {
     private Pane tile00, tile01, tile02, tile03, tile04, tile05, tile06, tile07, tile10, tile11, tile12, tile13, tile14, tile15, tile16, tile17, tile20, tile21, tile22, tile23, tile24, tile25, tile26, tile27, tile30, tile31, tile32, tile33, tile34, tile35, tile36, tile37, tile40, tile41, tile42, tile43, tile44, tile45, tile46, tile47, tile50, tile51, tile52, tile53, tile54, tile55, tile56, tile57, tile60, tile61, tile62, tile63, tile64, tile65, tile66, tile67, tile70, tile71, tile72, tile73, tile74, tile75, tile76, tile77;
     
     
+    private char currentPlayerTurn = 'w';
+    private boolean hasPieceBeenChosen = false;
+    private Piece chosenPiece;
     private String pieceSprite;
     private ImageView prevImageView;
-    private boolean turnOver = false;
+    // private boolean turnOver = false;
+    private Game game = new Game();
+    private ArrayList<String> legalMovesStrings;
 
 
     @FXML
-    void testClick(MouseEvent event) {
-        
-        ImageView iView = (ImageView)event.getSource();
-        Image piece;
-        try {
+    private void testClick (MouseEvent event) {
+        if (!hasPieceBeenChosen){
+            ImageView iView = (ImageView)event.getSource();
+            Image piece;
+            
             piece = iView.getImage();
+            
+            if (piece == null)
+                return;
 
-        } catch (NullPointerException e) {
+            pieceSprite = piece.getUrl();
+            char blackOrWhite =  pieceSprite.charAt(pieceSprite.length() - 6);
+            if (blackOrWhite != this.currentPlayerTurn) {
+                return;
+            }
+
+            int row = GridPane.getRowIndex(iView);
+            int col = GridPane.getColumnIndex(iView);
+            
+            System.out.println(row + " " + col);
+
+            legalMovesStrings = game.getLegalMoves(row, col);
+            hasPieceBeenChosen = true;
+
+        }
+
+        ImageView iView = (ImageView)event.getSource();
+        String legalMoveId = null;
+        
+        for (String coordinate : legalMovesStrings) {
+            if (coordinate.equals(iView.getId())) {
+                legalMoveId = coordinate;
+                break;
+            }
+        }
+
+        if (legalMoveId == null)
             return;
-        }
-
-        //file:/C:/School/oop/TDT4100_prosjekt_ingara/target/classes/project/sprites/wPawn.png
-        pieceSprite =  piece.getUrl();
-
-        // Send which tile was clicked to Game and get the legal moves in return.
-
-        }
-
-
-
-    public ChessController()
-    {
+        
+        
     }
+
+    private void drawCirclesForLegalMoves(ArrayList<String> legalMovesStrings) {
+        for (String legalMove : legalMovesStrings) {
+            ImageView legalMoveImageView = (ImageView)sprites.lookup("#" + legalMove);
+            Circle circle = new Circle(0, 0, 50);
+            
+            
+            
+            int row = GridPane.getRowIndex(legalMoveImageView);
+            int col = GridPane.getColumnIndex(legalMoveImageView);
+
+            tileColors.add(circle, col, row);
+        }
+    }
+
+    private void getCoordinatesFromString() {
+        
+    }
+
 
     private void initialize()
     {
+    }
+
+
+    public static void main(String[] args) {
+
     }
 }
--- src/main/java/project/Game.java
+++ src/main/java/project/Game.java
@@ -1,5 +1,72 @@
 package project;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Set;
+
+import project.Board.Chessboard;
+import project.Board.Tile;
+import project.Movement.TileCheckLegalMoves;
+import project.Pieces.Piece;
+
 public class Game {
     
+    Chessboard chessboard;
+    TileCheckLegalMoves checkLegalMoves;
+    Tile[][] currentGamePositionTiles;
+
+    public Game() {
+        this.chessboard               = new Chessboard();        //Only necessary for printing board 
+        this.currentGamePositionTiles = chessboard.getBoardTiles();
+        this.checkLegalMoves          = new TileCheckLegalMoves(currentGamePositionTiles);  
+    }
+
+    public ArrayList<String> getLegalMoves(int row, int col) {
+
+        HashMap<Tile, ArrayList<int[]>> allLegalMovesAfterControl = checkLegalMoves.CheckforCheckMateAndPat();
+        Tile tilePieceToMoveIsOn = currentGamePositionTiles[row][col]; 
+        Piece pieceToMove = this.currentGamePositionTiles[row][col].getPiece();
+        ArrayList<int[]> legalMovesForPieceToMove = allLegalMovesAfterControl.get(tilePieceToMoveIsOn);
+
+        //TODO: check if there are no legal moves and return nothing if so
+
+        return new ArrayList<String>(coordinateArrayToString(legalMovesForPieceToMove));
+    }
+
+    private ArrayList<String> coordinateArrayToString(ArrayList<int[]> coordinateArray) {
+        
+        ArrayList<String> coordinateString = new ArrayList<String>();
+
+        for (int[] legalMove : coordinateArray) {
+            coordinateString.add(legalMove[0] + "" + legalMove[1]);
+        }
+        
+        return coordinateString;
+    }
+
+
+    public static void main(String[] args) {
+                
+        String legalMovesString = "";
+        ArrayList<int[]> test = new ArrayList<int[]>() {
+            {
+                add(new int[] {1, 2});
+                add(new int[] {4, 3});
+            }
+        };
+
+
+
+
+        int[] yo= new int[] {1, 2};
+        test.add(yo);
+        for (int[] legalMove : test) {
+            legalMovesString += legalMove[0];
+            legalMovesString += legalMove[1];
+        }
+
+        System.out.println(legalMovesString);
+        
+    }
+
 }
