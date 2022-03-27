4f30a2e3bd22b6de869b68d77111c9a6334332a9
Ingar Solveigson Asheim <ingara@stud.ntnu.no>
2022-03-27 16:33:55 +0200

    Added autogeneration of tile colors.

---
+++ .VSCodeCounter/2022-03-27_13-40-01/results.md
@@ -0,0 +1,42 @@
+# Summary
+
+Date : 2022-03-27 13:40:01
+
+Directory c:\School\oop_project\TDT4100_prosjekt_ingara
+
+Total : 33 files,  3213 codes, 387 comments, 1016 blanks, all 4616 lines
+
+summary / [details](details.md) / [diff summary](diff.md) / [diff details](diff-details.md)
+
+## Languages
+| language | files | code | comment | blank | total |
+| :--- | ---: | ---: | ---: | ---: | ---: |
+| Java | 24 | 2,282 | 372 | 884 | 3,538 |
+| XML | 3 | 731 | 1 | 17 | 749 |
+| Generic Pseudocode | 4 | 128 | 14 | 80 | 222 |
+| Markdown | 1 | 60 | 0 | 29 | 89 |
+| Python | 1 | 12 | 0 | 6 | 18 |
+
+## Directories
+| path | files | code | comment | blank | total |
+| :--- | ---: | ---: | ---: | ---: | ---: |
+| . | 33 | 3,213 | 387 | 1,016 | 4,616 |
+| src | 31 | 3,106 | 386 | 977 | 4,469 |
+| src\main | 29 | 3,069 | 386 | 962 | 4,417 |
+| src\main\java | 27 | 2,385 | 386 | 955 | 3,726 |
+| src\main\java\exampleproject | 3 | 53 | 22 | 19 | 94 |
+| src\main\java\project | 23 | 2,326 | 364 | 935 | 3,625 |
+| src\main\java\project\Board | 2 | 189 | 6 | 67 | 262 |
+| src\main\java\project\Files | 1 | 3 | 3 | 5 | 11 |
+| src\main\java\project\Movement | 4 | 1,373 | 266 | 544 | 2,183 |
+| src\main\java\project\Pieces | 7 | 100 | 6 | 48 | 154 |
+| src\main\java\project\pseudocode | 4 | 128 | 14 | 80 | 222 |
+| src\main\resources | 2 | 684 | 0 | 7 | 691 |
+| src\main\resources\exampleproject | 1 | 18 | 0 | 3 | 21 |
+| src\main\resources\project | 1 | 666 | 0 | 4 | 670 |
+| src\python | 1 | 12 | 0 | 6 | 18 |
+| src\test | 1 | 25 | 0 | 9 | 34 |
+| src\test\java | 1 | 25 | 0 | 9 | 34 |
+| src\test\java\exampleproject | 1 | 25 | 0 | 9 | 34 |
+
+summary / [details](details.md) / [diff summary](diff.md) / [diff details](diff-details.md)
--- src/main/java/project/ChessController.java
+++ src/main/java/project/ChessController.java
@@ -12,7 +12,10 @@ import javafx.scene.input.MouseButton;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
+import javafx.scene.paint.Paint;
 import javafx.scene.shape.Circle;
+import javafx.scene.shape.Rectangle;
 import project.Pieces.Piece;
 
 /*
@@ -88,9 +91,6 @@ public class ChessController {
     private ImageView sprite00, sprite01, sprite02, sprite03, sprite04, sprite05, sprite06, sprite07, sprite10, sprite11, sprite12, sprite13, sprite14, sprite15, sprite16, sprite17, sprite20, sprite21, sprite22, sprite23, sprite24, sprite25, sprite26, sprite27, sprite30, sprite31, sprite32, sprite33, sprite34, sprite35, sprite36, sprite37, sprite40, sprite41, sprite42, sprite43, sprite44, sprite45, sprite46, sprite47, sprite50, sprite51, sprite52, sprite53, sprite54, sprite55, sprite56, sprite57, sprite60, sprite61, sprite62, sprite63, sprite64, sprite65, sprite66, sprite67, sprite70, sprite71, sprite72, sprite73, sprite74, sprite75, sprite76, sprite77;
     
     
-    @FXML
-    private Pane tile00, tile01, tile02, tile03, tile04, tile05, tile06, tile07, tile10, tile11, tile12, tile13, tile14, tile15, tile16, tile17, tile20, tile21, tile22, tile23, tile24, tile25, tile26, tile27, tile30, tile31, tile32, tile33, tile34, tile35, tile36, tile37, tile40, tile41, tile42, tile43, tile44, tile45, tile46, tile47, tile50, tile51, tile52, tile53, tile54, tile55, tile56, tile57, tile60, tile61, tile62, tile63, tile64, tile65, tile66, tile67, tile70, tile71, tile72, tile73, tile74, tile75, tile76, tile77;
-    
     //private Piece chosenPiece;
     //private char currentPlayerTurn = 'w';
     // private boolean turnOver = false;
@@ -98,9 +98,27 @@ public class ChessController {
     private boolean pieceHasBeenChosen = false;
     private String chosenPieceSpriteUrl;
     private ImageView chosenPieceImageView;
-    private Game game = new Game();
     private ArrayList<String> legalMovesStrings;
+    private Game game = new Game();
 
+    @FXML
+    private void colorTiles() {
+
+        Color dark =  new Color(81.0/255, 42.0/255, 42.0/255, 1);
+        Color light = new Color(122.0/255, 74.0/255, 61.0/255, 1);
+        Paint tileColor;
+
+        for (int row = 0; row < tileColors.getRowCount(); ++row ) {
+            for (int col = 0; col < tileColors.getColumnCount(); ++col) {
+                
+                tileColor = (row % 2 == 1) ?
+                            (col % 2 == 1) ? dark : light
+                           :(col % 2 == 1) ? light : dark;
+
+                tileColors.add(new Rectangle(100, 100, tileColor), col, row); 
+            }
+        }
+    }
 
     @FXML
     private void testClick (MouseEvent event) {
@@ -172,13 +190,10 @@ public class ChessController {
         }
     }
 
-    private void getCoordinatesFromString() {
-        
-    }
-
     @FXML
     private void initialize()
     {
+        colorTiles();
     }
 
 
